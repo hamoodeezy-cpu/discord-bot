35e6082fc08f1c1b1aaf7ba32f18f0df13f686bf
hamoodeezy-cpu <hamoodeezy@gmail.com>
2026-07-01 08:56:57 -0500

    Update infraction.js

--- commands/infraction.js
+++ commands/infraction.js
@@ -1,1 +1,17 @@
+const fs = require("fs");
+const path = require("path");
 
+const database = path.join(__dirname, "..", "data", "infractions.json");
+
+function loadData() {
+    return JSON.parse(fs.readFileSync(database, "utf8"));
+}
+
+function saveData(data) {
+    fs.writeFileSync(database, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+    loadData,
+    saveData
+};
