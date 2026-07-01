a38348ff72aefda0cbb7f87318077a204f61a5a3
hamoodeezy-cpu <hamoodeezy@gmail.com>
2026-07-01 15:12:55 -0500

    Update brm5.js

--- commands/brm5.js
+++ commands/brm5.js
@@ -14,20 +14,22 @@ module.exports = {
   async execute(interaction) {
     const code = interaction.options.getString('code');
 
-    const message = `
-## *BRM5 Deployment*
+    const message = `## *BRM5 Deployment*
 
 Hello <@&1313620498768203827> , <@&1519889298785304687> , <@&1519889157927731270>
 
 A BRM5 deployment is being hosted in 15 minutes, start joining now! Wait for further instructions in https://discordapp.com/channels/1302283181998997616/1512985724948054236
 
-Server Code Is: \`\`\`${code}\`\`\`
+Server Code Is:
+\`\`\`${code}\`\`\`
+
 In-game: Red Squad
 
-***React to this message if attending!!!***
-`;
+***React to this message if attending!!!***`;
 
-    const msg = await interaction.channel.send({ content: message });
+    const msg = await interaction.channel.send({
+      content: message
+    });
 
     await msg.react('✅');
 
